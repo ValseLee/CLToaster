16dac8184e497fcc1b18c280e853aaca61e3316b
valselee <sollleky72@gmail.com>
2024-01-22 23:24:14 +0900

    [init] Demo Project Init

--- Demo/Demo/VC/ViewController.swift
+++ Demo/Demo/VC/ViewController.swift
@@ -9,12 +9,18 @@ import CLToast
 import UIKit
 
 final class CLToastDemoVC: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    view.backgroundColor = .yellow
+    presentToast()
   }
 
-
+  private func presentToast() {
+    let toastManager = CLToast<OverViewController>(
+      with: CLToastInfo(icon: .checkmark, message: "Hi")
+    )
+    
+    toastManager.present(in: self)
+  }
 }
 
