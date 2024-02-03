ad1da059a86d28b0efee71516f71956dafe38251
valselee <sollleky72@gmail.com>
2024-02-03 18:46:01 +0900

    [Feat] 1. CLToastPresentable protocol added 2. be updated soon

--- CLToast/Sources/CLToast/Protocols/CLToastPresentable.swift
+++ CLToast/Sources/CLToast/Protocols/CLToastPresentable.swift
@@ -7,12 +7,35 @@
 
 import UIKit
 
-protocol ViewLayerConfigurable {
-  func configAnimation()
-  func configDropShadow()
-  func configVerticalLayout()
-}
-
 protocol CLToastPresentable: UIViewController {
   var onDismiss: (() -> Void)? { get set }
+  var style: CLToastStyle? { get set }
+  func animate()
+  func dismissCLToast()
+}
+
+extension CLToastPresentable {
+  internal func animate() {
+    UIView.animate(withDuration: 0.35, delay: 0.0, options: .curveEaseOut) { [weak self] in
+      guard let self else { return }
+      if let toastView = self.view.subviews.first {
+        toastView.frame.origin.y += 40
+        toastView.layer.opacity = 1.0
+      }
+    } completion: { isAnimated in
+      DispatchQueue.main.asyncAfter(deadline: .now() + 1.25) { [weak self] in
+        guard let self else { return }
+        self.dismissCLToast()
+      }
+    }
+  }
+  
+  private func removeVC(isAnimated: Bool) {
+    if isAnimated,
+       self.parent != nil {
+      self.removeFromParent()
+      self.view.removeFromSuperview()
+      self.dismiss(animated: true, completion: onDismiss)
+    }
+  }
 }
