f99efb379f61a32c4b3819febafe77915508d92c
valselee <sollleky72@gmail.com>
2024-02-04 18:55:19 +0900

    [Update/#1] 1. CLToast가 Package의 endpoint 로 정리됨 2. CLToast를 생성할 때 Generic Type에 의해 어떤 Toast를 보여줄 것인지 타입으로 지정하도록 구현

--- CLToast/Sources/CLToast/CLToast.swift
+++ CLToast/Sources/CLToast/CLToast.swift
@@ -1,64 +1,29 @@
 import UIKit
 
+// MARK: Present Style Constraint
 public protocol CLToastPresentStyle { }
 public enum OverViewController: CLToastPresentStyle { }
 public enum OverNavigationBar: CLToastPresentStyle { }
 
-public struct CLToast<Present: CLToastPresentStyle> {
-  private var toastPresentable: CLToastPresentable
-  
-  private func removeToastPresentable(isAnimated: Bool) {
-    if isAnimated,
-       toastPresentable.parent != nil {
-      toastPresentable.removeFromParent()
-      toastPresentable.view.removeFromSuperview()
-      toastPresentable.dismiss(animated: true, completion: toastPresentable.onDismiss)
-    }
+// MARK: Package Endpoint
+open class CLToast<PresentOver: CLToastPresentStyle> {
+  public weak var toastController: CLToastPresentable?
+}
+
+extension CLToast where PresentOver == OverNavigationBar {
+  public func present(in parent: UIViewController) {
+    guard
+      let toastController,
+      let nav = parent.navigationController else { return }
+    nav.beginAppearanceTransition(true, animated: true)
+    toastController.present(in: nav)
+    nav.endAppearanceTransition()
   }
 }
 
-extension CLToast where Present == OverViewController {
-  public func present(
-    in vc: UIViewController,
-    onDismiss: (() -> Void)? = { }
-  ) {
-    if let onDismiss { toastPresentable.onDismiss = onDismiss }
-    present(in: vc)
-  }
-  
-  public func present(in vc: UIViewController) {
-    Task { @MainActor [weak vc] in
-      guard let vc else { return }
-      vc.addChild(toastPresentable)
-      vc.view.addSubview(toastPresentable.view)
-      toastPresentable.didMove(toParent: vc)
-      toastPresentable.view.frame = vc.view.bounds
-    }
-  }
-}
-
-extension CLToast where Present == OverNavigationBar {
-  public func present(
-    in vc: UIViewController,
-    onDismiss: (() -> Void)? = { }
-  ) {
-    if let onDismiss { toastPresentable.onDismiss = onDismiss }
-    present(in: vc)
-  }
-  
-  public func present(
-    in vc: UIViewController
-  ) {
-    Task { @MainActor [weak vc] in
-      guard
-        let vc,
-        let navigationController = vc.navigationController else { return }
-      navigationController.addChild(toastPresentable)
-      navigationController.navigationBar.addSubview(toastPresentable.view)
-      navigationController.beginAppearanceTransition(true, animated: true)
-      navigationController.endAppearanceTransition()
-      toastPresentable.didMove(toParent: navigationController)
-      toastPresentable.view.frame = navigationController.navigationBar.bounds
-    }
+extension CLToast where PresentOver == OverViewController {
+  public func present(in parent: UIViewController) {
+    guard let toastController else { return }
+    toastController.present(in: parent)
   }
 }
