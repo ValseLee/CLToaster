2c315911e03080d56145593b0b296171d9c53047
valselee <sollleky72@gmail.com>
2024-02-05 14:53:32 +0900

    [Refactor/#1] 1. toastController 가 갖고 있던 present를 외부로 꺼내서 로직 작동 확인 2. 추후 navigation Present, VC Present 를 분기처리해서 다시 책임 위임 예정

--- CLToast/Sources/CLToast/CLToast.swift
+++ CLToast/Sources/CLToast/CLToast.swift
@@ -7,23 +7,44 @@ public enum OverNavigationBar: CLToastPresentStyle { }
 
 // MARK: Package Endpoint
 open class CLToast<PresentOver: CLToastPresentStyle> {
-  public weak var toastController: CLToastPresentable?
+  public var toastController: CLToastPresentable?
+  
+  public init(toastController: CLToastPresentable) {
+    self.toastController = toastController
+  }
+  
+  public init() {
+    let defaultVC = CLToastDefaultVC()
+    let defaultAnimation = CLToastAnimateClient()
+    defaultVC.animationDelegate = defaultAnimation
+    self.toastController = defaultVC
+  }
 }
 
 extension CLToast where PresentOver == OverNavigationBar {
   public func present(in parent: UIViewController) {
     guard
       let toastController,
-      let nav = parent.navigationController else { return }
+      let nav = parent.navigationController,
+      !nav.isNavigationBarHidden else { return }
+    nav.addChild(toastController)
+    nav.navigationBar.addSubview(toastController.view)
+
     nav.beginAppearanceTransition(true, animated: true)
-    toastController.present(in: nav)
     nav.endAppearanceTransition()
+    
+    toastController.didMove(toParent: nav)
+    toastController.view.frame = nav.navigationBar.bounds
   }
 }
 
 extension CLToast where PresentOver == OverViewController {
   public func present(in parent: UIViewController) {
     guard let toastController else { return }
-    toastController.present(in: parent)
+    parent.addChild(toastController)
+    parent.view.addSubview(toastController.view)
+    
+    toastController.didMove(toParent: parent)
+    toastController.view.frame.size = CGSize(width: parent.view.bounds.width, height: 150)
   }
 }
