e4ac8c406f20a179dd2b4ccb66b6af60a0176088
valselee <sollleky72@gmail.com>
2024-02-07 17:28:44 +0900

    [Feat/#] 1. TosatView를 사용자가 원하는 UIView로 설정할 때, 그 자신을 구성하는 childView 들을 toastView 내부에서 접근할 수 있도록 dynamicMemberLookup 을 지원하는 프로토콜 구현 2. FieldContainer는 ToastView의 내부 View와 속성에 접근할 수 있도록 지원하는 역할을 수행하며, Builder에게 자기 자신을 인자로 전달하여 원하는 View만 빌드할 수 있도록 함

--- CLToast/Sources/CLToast/Protocols/CLToastViewField.swift
+++ CLToast/Sources/CLToast/Protocols/CLToastViewField.swift
@@ -1,8 +1,29 @@
 //
-//  File.swift
-//  
+//  CLToastViewField.swift
+//
 //
 //  Created by Celan on 2/6/24.
 //
 
-import Foundation
+import UIKit
+
+@dynamicMemberLookup
+protocol CLToastViewField {
+  associatedtype CLToastViewType: UIView
+  var toastView: CLToastViewType { get set }
+  init(of toastView: CLToastViewType)
+  subscript(dynamicMember dynamicMember: KeyPath<CLToastViewType, UIView>) -> UIView { get }
+}
+
+struct CLToastViewFieldContainer<T: UIView>: CLToastViewField {
+  typealias CLToastViewType = T
+  var toastView: CLToastViewType
+  
+  init(of toastView: T) {
+    self.toastView = toastView
+  }
+  
+  subscript(dynamicMember dynamicMember: KeyPath<CLToastViewType, UIView>) -> UIView {
+    return toastView[keyPath: dynamicMember]
+  }
+}
