9ec49daa0cba001d3939ca36d1892bd7a80b6753
valselee <sollleky72@gmail.com>
2024-02-09 22:01:21 +0900

    [Files/#8] 1. CGSize 확장의 디렉토리 이동

---
+++ CLToast/Sources/CLToast/Utilities/Extensions/CGSize+imageView.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Celan on 1/22/24.
+//
+
+import UIKit
+
+extension CGSize {
+  public enum IconSizes {
+    public static var `default` = CGSize(width: 50, height: 50)
+    /// 20 x 20
+    public static var small = CGSize(width: 20, height: 20)
+    /// 24 x 24
+    public static var medium = CGSize(width: 24, height: 24)
+    /// 36 x 36
+    public static var large = CGSize(width: 36, height: 36)
+  }
+}
