1bac544a270ae5b65f6bf32f634e0904be7e4923
valselee <sollleky72@gmail.com>
2024-02-10 12:37:24 +0900

    [Feat/#8] 1. CLToastLayer를 관리하는 클라이언트 구현 2. UIKit과 SwiftUI를 동시에 만족할 수 있는 인터페이스 구현 필요

--- CLToast/Sources/CLToast/Protocols/CLToastViewLayerManageable.swift
+++ CLToast/Sources/CLToast/Protocols/CLToastViewLayerManageable.swift
@@ -1,10 +1,20 @@
 //
-//  File.swift
-//  
+//  CLToastViewLayerManageable.swift
+//
 //
 //  Created by Celan on 2/9/24.
 //
 
 import UIKit
 
+protocol CLToastViewLayerManageable {
+  func configLayer(for view: UIView, with style: CLToastStyle)
+}
 
+struct CLToastViewLayerClient: CLToastViewLayerManageable {
+  func configLayer(for view: UIView, with style: CLToastStyle) {
+    view.layer.opacity = style.layerOpacity
+    view.layer.cornerRadius = style.layerCornerRadius
+    view.backgroundColor = style.backgroundColor
+  }
+}
