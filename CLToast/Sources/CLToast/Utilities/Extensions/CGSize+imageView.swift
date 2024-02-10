29db9feda37c5530b7f49703b774291445917309
valselee <sollleky72@gmail.com>
2024-02-10 16:29:24 +0900

    [Update/#8] 1. Icon Size를 수정(large == 75, default == 50에 맞도록) 2. Icon Size가 large일 때, toastView의 height가 작을 수도..

--- CLToast/Sources/CLToast/Utilities/Extensions/CGSize+imageView.swift
+++ CLToast/Sources/CLToast/Utilities/Extensions/CGSize+imageView.swift
@@ -11,10 +11,10 @@ extension CGSize {
   public enum IconSizes {
     public static var `default` = CGSize(width: 50, height: 50)
     /// 20 x 20
-    public static var small = CGSize(width: 20, height: 20)
+    public static var small = CGSize(width: 24, height: 24)
     /// 24 x 24
-    public static var medium = CGSize(width: 24, height: 24)
+    public static var medium = CGSize(width: 40, height: 40)
     /// 36 x 36
-    public static var large = CGSize(width: 36, height: 36)
+    public static var large = CGSize(width: 75, height: 75)
   }
 }
