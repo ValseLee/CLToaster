c5aa0fe6057ce8c2a6e067ed089c3d5342ffe552
valselee <sollleky72@gmail.com>
2024-02-10 16:23:12 +0900

    [Feat/#8] 1. Toast Demo를 위한 VC 구현 2. basic, detailed, bottom, customAnimation 4개 기능 구현 3. animation 쪽에서 queue 처리가 되지 않아서 쌓이는 이슈가 있으며, 수정 예정..

--- Demo/Demo/VC/ViewController.swift
+++ Demo/Demo/VC/ViewController.swift
@@ -9,38 +9,155 @@ import CLToast
 import UIKit
 
 final class CLToastDemoVC: UIViewController {
-  let button: UIButton = {
-    let btn = UIButton()
-    btn.setTitle("Present", for: .normal)
-    btn.setTitleColor(.systemPink, for: .normal)
-    return btn
-  }()
+  var detailedToastPresentButton: UIButton = UIButton()
+  var basicToastPresentButton: UIButton = UIButton()
+  var customizedAnimationToastPresentButton: UIButton = UIButton()
+  var bottomToastPresentButton: UIButton = UIButton()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     title = "MAIN"
-    view.backgroundColor = .yellow
+    view.backgroundColor = .systemBackground
     
-    view.addSubview(button)
-    button.addTarget(self, action: #selector(presentToast), for: .touchUpInside)
-    button.translatesAutoresizingMaskIntoConstraints = false
-    
-    NSLayoutConstraint.activate([
-      button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-      button.heightAnchor.constraint(equalToConstant: 100),
-      button.widthAnchor.constraint(equalToConstant: 100),
-    ])
+    makeDetailedToastPresentButton()
+    makeBasicToastPresentButton()
+    makeCustomizedAnimationToastPresentButton()
+    makeBottomToastPresentButton()
   }
   
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
   }
   
+  private func makeDetailedToastPresentButton() {
+    detailedToastPresentButton.setTitle("Detail Toast", for: .normal)
+    detailedToastPresentButton.setTitleColor(.systemPink, for: .normal)
+    
+    view.addSubview(detailedToastPresentButton)
+    detailedToastPresentButton.addTarget(self, action: #selector(presentDetailedToast), for: .touchUpInside)
+    detailedToastPresentButton.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      detailedToastPresentButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      detailedToastPresentButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      detailedToastPresentButton.heightAnchor.constraint(equalToConstant: 50),
+      detailedToastPresentButton.widthAnchor.constraint(equalToConstant: 200),
+    ])
+  }
+  
+  private func makeBasicToastPresentButton () {
+    basicToastPresentButton.setTitle("Basic", for: .normal)
+    basicToastPresentButton.setTitleColor(.systemGreen, for: .normal)
+    
+    view.addSubview(basicToastPresentButton)
+    basicToastPresentButton.addTarget(self, action: #selector(presentBasicToast), for: .touchUpInside)
+    basicToastPresentButton.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      basicToastPresentButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      basicToastPresentButton.bottomAnchor.constraint(equalTo: detailedToastPresentButton.topAnchor, constant: -20),
+      basicToastPresentButton.heightAnchor.constraint(equalToConstant: 50),
+      basicToastPresentButton.widthAnchor.constraint(equalToConstant: 200),
+    ])
+  }
+  
+  private func makeCustomizedAnimationToastPresentButton() {
+    customizedAnimationToastPresentButton.setTitle("Animation Custom", for: .normal)
+    customizedAnimationToastPresentButton.setTitleColor(.systemBlue, for: .normal)
+    
+    view.addSubview(customizedAnimationToastPresentButton)
+    customizedAnimationToastPresentButton.addTarget(self, action: #selector(presentToastWithCustomizeAnimation), for: .touchUpInside)
+    customizedAnimationToastPresentButton.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      customizedAnimationToastPresentButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      customizedAnimationToastPresentButton.topAnchor.constraint(equalTo: detailedToastPresentButton.bottomAnchor, constant: 20),
+      customizedAnimationToastPresentButton.heightAnchor.constraint(equalToConstant: 50),
+      customizedAnimationToastPresentButton.widthAnchor.constraint(equalToConstant: 200),
+    ])
+  }
+  
+  private func makeBottomToastPresentButton() {
+    bottomToastPresentButton.setTitle("Bottom Toast", for: .normal)
+    bottomToastPresentButton.setTitleColor(.systemBrown, for: .normal)
+    
+    view.addSubview(bottomToastPresentButton)
+    bottomToastPresentButton.addTarget(self, action: #selector(presentToastFromBottom), for: .touchUpInside)
+    bottomToastPresentButton.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      bottomToastPresentButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      bottomToastPresentButton.topAnchor.constraint(equalTo: customizedAnimationToastPresentButton.bottomAnchor, constant: 20),
+      bottomToastPresentButton.heightAnchor.constraint(equalToConstant: 50),
+      bottomToastPresentButton.widthAnchor.constraint(equalToConstant: 200),
+    ])
+  }
+  
   @objc
-  private func presentToast() {
-    let navigationToast = CLToast()
-    navigationToast.present(in: self)
-//    navigationToast.present(in: self)
+  private func presentDetailedToast() {
+    let style = CLToastStyleBuilder("Title")
+      .buildValue(\.description, newValue: "Description")
+      .buildValue(\.timeline, newValue: Date().formatted())
+      .buildValue(\.image, newValue: .actions)
+      .buildStyle()
+    
+    CLToast(with: style) { [weak self] in
+      self?.completionHandler()
+    }
+    .present(in: view)
+  }
+  
+  @objc
+  private func presentBasicToast() {
+    CLToast(title: "Title", height: 50, displayDirection: .top)
+      .present(in: view)
+  }
+  
+  @objc
+  private func presentToastWithCustomizeAnimation() {
+    let style = CLToastStyleBuilder("Title")
+      .buildValue(\.animateSpeed, newValue: 1)
+      .buildStyle()
+    
+    CLToast(with: style,animationManager: MyAnimationManager(style: style))
+      .present(in: view)
+  }
+  
+  @objc
+  private func presentToastFromBottom() {
+    let style = CLToastStyleBuilder("Bottom Toast")
+      .buildValue(\.description, newValue: "Description Here")
+      .buildValue(\.timeline, newValue: Date().formatted())
+      .buildValue(\.displayFrom, newValue: .bottom)
+      .buildStyle()
+    
+    CLToast(with: style)
+      .present(in: view)
+  }
+  
+  func completionHandler() { print(#function) }
+}
+
+struct MyAnimationManager: CLToastAnimatable {
+  let style: CLToastStyle
+  
+  func animate(
+    for view: UIView,
+    completion: @escaping () -> Void
+  ) {
+    UIView.animate(withDuration: style.animateSpeed) {
+      view.frame.origin.x += 20
+    } completion: { isAnimated in
+      if isAnimated {
+        UIView.animate(
+          withDuration: style.animateSpeed,
+          delay: style.displayTimeInterval
+        ) {
+          view.frame.origin.y -= style.animateY
+        } completion: { isAnimated in
+          if isAnimated { view.removeFromSuperview() }
+        }
+      }
+    }
   }
 }
