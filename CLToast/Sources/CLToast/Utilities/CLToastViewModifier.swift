f6736a68d14b06d091d7f3e9676092ae7a1cba58
valselee <sollleky72@gmail.com>
2024-02-12 23:28:39 +0900

    [Feat/#10] 1. SwiftUI: CLToast로 동일하게 대응하고 싶었으나.. View에서 직접 호출하고 자기 자신을 wrapping 해야 하는 관계로 View Modifier로 정리 2. Animation과 Component들을 바탕으로 기본 템플릿 구현 완료 3. Animation Delegate로 대응하기 위해 추후 프로토콜 정리가 필요

--- CLToast/Sources/CLToast/Utilities/CLToastViewModifier.swift
+++ CLToast/Sources/CLToast/Utilities/CLToastViewModifier.swift
@@ -1,12 +1,166 @@
 //
-//  File.swift
-//  
+//  CLToastViewModifier.swift
+//
 //
 //  Created by Celan on 2/12/24.
 //
 
 import SwiftUI
 
-//struct CLToastViewModifier: ViewModifier {
-//  
-//}
+public struct CLToastViewModifier: ViewModifier {
+  @Binding var isPresented: Bool
+  @State private var isPresenting: Bool = false
+  let onDismiss: (() -> Void)?
+  let style: CLToastStyle
+  
+  var toastPresentSection: Alignment {
+    switch style.displayFrom {
+    case .top: return .top
+    case .bottom: return .bottom
+    case .center: return .center
+    }
+  }
+  
+  public init(
+    isPresented: Binding<Bool>,
+    style: CLToastStyle,
+    onDismiss: (() -> Void)? = nil
+  ) {
+    self.style = style
+    self._isPresented = isPresented
+    self.onDismiss = onDismiss
+  }
+  
+  public func body(content: Content) -> some View {
+    content
+      .onChange(of: isPresented) { newValue in
+        if style.isAnimationEnabled {
+          let animation = Animation.easeInOut(duration: style.animateSpeed)
+          withAnimation(animation) {
+            isPresenting = newValue
+          }
+        } else {
+          isPresenting = newValue
+        }
+      }
+      .overlay(alignment: toastPresentSection) {
+        if isPresenting {
+          buildToastView()
+        }
+      }
+  }
+}
+
+// MARK: - ViewModifier as ViewBuilder
+extension CLToastViewModifier: CLToastViewBuildDelegate {
+  var titleView: some View {
+    Text(style.title)
+      .foregroundColor(Color(uiColor: .label))
+      .font(.title2)
+  }
+  
+  var descriptionView: (some View)? {
+    if let description = style.description {
+      Text(description)
+        .foregroundColor(Color(uiColor: .label))
+        .font(.footnote)
+      
+    } else { nil }
+  }
+  
+  var timelineView: (some View)? {
+    if let timeline = style.timeline {
+      Text(timeline)
+        .foregroundColor(Color(uiColor: .secondaryLabel))
+        .font(.caption)
+        .frame(
+          maxWidth: .infinity,
+          maxHeight: .infinity,
+          alignment: .bottomTrailing
+        )
+        .padding([.trailing, .bottom], 16)
+      
+    } else { nil }
+  }
+  
+  var imageView: (some View)? {
+    if let image = style.image {
+      Image(uiImage: image)
+        .resizable()
+      
+    } else { nil }
+  }
+  
+  @ViewBuilder
+  public func buildToastView() -> (some View)? {
+      ZStack {
+        RoundedRectangle(cornerRadius: style.layerCornerRadius)
+          .fill(Color(uiColor: style.backgroundColor))
+        
+        if let imageView {
+          HStack(
+            alignment: .center,
+            spacing: 0
+          ) {
+            imageView
+              .frame(
+                width: style.imageSize.width,
+                height: style.imageSize.height
+              )
+              .aspectRatio(contentMode: .fit)
+              .padding(.leading, 16)
+            
+            VStack(spacing: 0) {
+              titleView
+              
+              descriptionView
+            }
+            .padding(.horizontal, 16)
+            
+            Spacer()
+          }
+          
+          timelineView
+          
+        } else {
+          VStack(spacing: 0) {
+            titleView
+            
+            descriptionView
+          }
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .padding(.horizontal, 16)
+          
+          timelineView
+        }
+      }
+      .frame(height: style.height)
+      .frame(maxWidth: .infinity)
+      .padding(.horizontal, 16)
+      .transition(
+        .move(edge: .top)
+        .combined(with: .opacity)
+      )
+      .task {
+        let displayTime = style.displayTimeInterval + style.animateSpeed
+        try? await Task.sleep(nanoseconds: UInt64(displayTime * 1_000_000_000))
+        dismissToast()
+      }
+  }
+  
+  func dismissToast() {
+    defer {
+      if let onDismiss { onDismiss() }
+    }
+    
+    if style.isAnimationEnabled {
+      withAnimation {
+        isPresented = false
+        isPresenting = false
+      }
+    } else {
+      isPresented = false
+      isPresenting = false
+    }
+  }
+}
