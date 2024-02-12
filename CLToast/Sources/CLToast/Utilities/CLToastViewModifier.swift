304883965c85c9819bb562031e818e876443dcb2
valselee <sollleky72@gmail.com>
2024-02-12 17:05:10 +0900

    [Files/#10] 1. SwiftUI 대응을 위한 SwiftUI Demo View 파일 생성 2. SwiftUI View, ViewModifier 파일 생성

---
+++ CLToast/Sources/CLToast/Utilities/CLToastViewModifier.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Celan on 2/12/24.
+//
+
+import SwiftUI
+
+//struct CLToastViewModifier: ViewModifier {
+//  
+//}
---
+++ Demo/Demo/SwiftUI/MySwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  MySwiftUI.swift
+//  Demo
+//
+//  Created by Celan on 2/12/24.
+//
+
+import SwiftUI
+
+struct MySwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MySwiftUI()
+}
