77269976993fa9699b19bd9347f2ca9cffd3a965
valselee <sollleky72@gmail.com>
2024-02-14 23:07:28 +0800

    [Update/#10] 1. UIKit의 offset 계산 메소드와 SwiftUI의 tranisiton offset 계산 메소드를 각각 확장으로 구현

--- CLToast/Sources/CLToast/Protocols/CLToastAnimatable.swift
+++ CLToast/Sources/CLToast/Protocols/CLToastAnimatable.swift
@@ -6,72 +6,28 @@
 //
 
 import UIKit
+import SwiftUI
 
-public protocol CLToastAnimatable {
-  var style: CLToastStyle { get }
-  func animate(for view: UIView, completion: @escaping () -> Void)
+public protocol CLToastAnimation {
+  associatedtype CLAnimationInfo
+  var animator: (() -> Void)? { get set }
+  func makeAnimation(with style: CLToastStyle) -> CLAnimationInfo
 }
 
-struct CLToastAnimateClient: CLToastAnimatable {
-  let style: CLToastStyle
+extension CLToastAnimation {
+    func getAnimateOffset(from style: CLToastStyle) -> CGFloat {
+      switch style.displayFrom {
+      case .top: style.animateY
+      case .bottom: -style.animateY
+      case .center: style.animateY
+      }
+    }
   
-  /// Trigger toastView's Animation with given ``CLToastStyle``.
-  /// - Parameters:
-  ///   - view: 애니메이션을 동작할 Toast Message View를 전달합니다.
-  ///   - completion: 애니메이션이 완전히 종료된 후 호출할 메소드를 전달합니다.
-  func animate(for view: UIView, completion: @escaping () -> Void) {
-    guard style.isAnimationEnabled else { return }
-    let appearingAnimation = makeAppearingAnimation(for: view)
-    let disappearingAnimation = makeDisappearingAnimation(for: view, didDisappear: completion)
-    
-    appearingAnimation.addCompletion { currentState in
-      guard case .end = currentState else { return }
-      disappearingAnimation.startAnimation(afterDelay: style.displayTimeInterval)
-    }
-    
-    appearingAnimation.startAnimation()
-  }
-}
-
-fileprivate extension CLToastAnimateClient {
-  func makeAppearingAnimation(for toastView: UIView) -> UIViewPropertyAnimator {
-    UIViewPropertyAnimator(
-      duration: style.animateSpeed,
-      curve: .easeInOut
-    ) {
-      toastView.frame.origin.y += getAnimateOffset()
-      toastView.layer.opacity = style.animateOpacity
-    }
-  }
-  
-  func makeDisappearingAnimation(
-    for toastView: UIView,
-    didDisappear: @escaping () -> Void
-  ) -> UIViewPropertyAnimator {
-    let animation = UIViewPropertyAnimator(
-      duration: style.animateSpeed,
-      curve: .easeInOut
-    ) {
-      toastView.frame.origin.y -= getAnimateOffset()
-      toastView.layer.opacity = 0.0
-    }
-    
-    animation.addCompletion { currentState in
-      guard case .end = currentState else { return }
-      didDisappear()
-      toastView.removeFromSuperview()
-    }
-    return animation
-  }
-  
-  func getAnimateOffset() -> CGFloat {
+  func getTransitionOffset(from style: CLToastStyle) -> CGFloat {
     switch style.displayFrom {
-    case .top:
-      style.animateY
-    case .bottom:
-      -style.animateY
-    case .center:
-      CGFloat.zero
+    case .top: -style.animateY
+    case .bottom: style.animateY
+    case .center: style.animateY
     }
   }
 }
