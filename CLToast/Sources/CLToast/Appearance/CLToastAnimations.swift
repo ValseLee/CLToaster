7b06879fa0ec9dca59b97ff4897a5c6dd8146b81
valselee <sollleky72@gmail.com>
2024-02-16 23:20:50 +0800

    [Feat/#10] 1. CLToastAnimation을 구성할 때 필요한 속성들을 분리하여 관리하는 구조체 구

--- CLToast/Sources/CLToast/Appearance/CLToastAnimations.swift
+++ CLToast/Sources/CLToast/Appearance/CLToastAnimations.swift
@@ -1,8 +1,60 @@
 //
-//  File.swift
-//  
+//  CLToastAnimations.swift
+//
 //
 //  Created by Celan on 2/16/24.
 //
 
-import Foundation
+import UIKit
+
+public enum CLToastDisplaySection {
+  case top, bottom, center
+}
+
+/**
+ - Properties: Animation
+ */
+public struct CLToastAnimations {
+  /**
+   Flag determines ToastView to animate.
+   Its value is ``true`` by default.
+   */
+  public var isAnimationEnabled = true
+  
+  /**
+   Display ToastView from given section value.
+   */
+  public var animateFrom: CLToastDisplaySection = .top
+  
+  /**
+   Display ToastView for its value.
+   */
+  public var displayTime: TimeInterval = 1.0
+  
+  /**
+   Animate ToastView's offset with its value.
+   
+   It automatically adjust offset's value depends on ``displayFrom``.
+   If ``displayFrom`` is ``.top``, ToastView will animate from top with
+   ``animateY`` value.
+   If ``displayFrom`` is ``.bottom``, ToastViwe will animate from bottom with ``animateY`` value.
+   */
+  public var offsetY: CGFloat = 40
+  
+  /**
+   Animate ToastView's opacity with its value.
+   Its value is ``1.0`` by default.
+   */
+  public var opacity: Float = 1.0
+  
+  /**
+   Animate ToastView with given speed.
+   */
+  public var animationSpeed: TimeInterval = 0.3
+  
+  /**
+   Initialize ``CLToastAnimation`` with default protperty value.
+   You can customize any properties with your taste with Proprety DI.
+   */
+  public init() { }
+}
