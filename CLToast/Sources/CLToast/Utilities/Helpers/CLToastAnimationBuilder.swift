0d1bf0e415088e35106cf880d1057f31c099662f
valselee <sollleky72@gmail.com>
2024-02-17 10:35:36 +0800

    [Feat/#10] 1. CLToastAnimationBuilder 를 구현 및 정의 2. 사용의 일관성을 위해 KeyPath로 빌드

--- CLToast/Sources/CLToast/Utilities/Helpers/CLToastAnimationBuilder.swift
+++ CLToast/Sources/CLToast/Utilities/Helpers/CLToastAnimationBuilder.swift
@@ -1,8 +1,24 @@
 //
-//  File.swift
-//  
+//  CLToastAnimationBuilder.swift
+//
 //
 //  Created by Celan on 2/17/24.
 //
 
-import Foundation
+public class CLToastAnimationBuilder {
+  var animation: CLToastAnimations
+  
+  public init() {
+    self.animation = CLToastAnimations()
+  }
+  
+  public func buildValue<Value>(
+    _ path: WritableKeyPath<CLToastAnimations,Value>,
+    into newValue: Value
+  ) -> Self {
+    animation[keyPath: path] = newValue
+    return self
+  }
+  
+  public func buildAnimation() -> CLToastAnimations { animation }
+}
