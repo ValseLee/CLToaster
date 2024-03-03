6ba9d3195e0779c84c540a43b5152017e1164280
valselee <sollleky72@gmail.com>
2024-03-03 23:56:55 +0900

    [Feat/#42] 1. UIKit에서 사용하는 Toast Data 타입 정의

--- Sources/CLToaster/UIKitToaster/Data/CLToastUIKitData.swift
+++ Sources/CLToaster/UIKitToaster/Data/CLToastUIKitData.swift
@@ -1,8 +1,25 @@
 //
-//  File.swift
-//  
+//  CLToastUIKitData.swift
+//
 //
 //  Created by Celan on 3/2/24.
 //
 
-import Foundation
+struct CLToastUIKitData {
+  var style: CLToastStyle
+  var animation: any CLToastUIKitAnimation
+  var display: CLToastDisplaySection = .bottom
+  var completion: (() -> Void)?
+  
+  init(
+    style: CLToastStyle,
+    animation: any CLToastUIKitAnimation = CLToastUIKitAnimationClient(toastAnimations: CLToastAnimations()),
+    display: CLToastDisplaySection,
+    completion: ( () -> Void)? = nil
+  ) {
+    self.style = style
+    self.animation = animation
+    self.display = display
+    self.completion = completion
+  }
+}
