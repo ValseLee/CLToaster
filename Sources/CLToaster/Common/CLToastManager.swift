8e6945ec98edc7fe54cfce84a29bbf42a03bce8a
valselee <sollleky72@gmail.com>
2024-03-26 13:23:17 +0900

    [Feat] 1. CLToastRepository 프로토콜 정의 2. PriorityQueue에서 데이터를 enqueue하고 dequeue하는 매니저 구현 3. 우선순위를 1,2,3 으로 분리한 후, UIKitData에서 채택한 Comparable 기준 힙 정렬 기능 구현

--- Sources/CLToaster/Common/CLToastManager.swift
+++ Sources/CLToaster/Common/CLToastManager.swift
@@ -1,8 +1,23 @@
 //
-//  File.swift
-//  
+//  CLToastManager.swift
+//
 //
 //  Created by Celan on 3/26/24.
 //
 
 import Foundation
+
+protocol CLToastRepository {
+  func dequeue() -> CLToastUIKitData?
+}
+
+final class CLToastManager: CLToastRepository {
+  fileprivate(set) var priorityQueue = PriorityQueue<CLToastUIKitData>(order: <)
+  func dequeue() -> CLToastUIKitData? {
+    priorityQueue.removeFirst()
+  }
+  
+  func enqueue(data: CLToastUIKitData) {
+    priorityQueue.push(data)
+  }
+}
