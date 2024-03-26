c03f68b5454c9b15f92f3285ab0edf5171d42d57
valselee <sollleky72@gmail.com>
2024-03-26 13:21:55 +0900

    [File/Feat] 1. Priority Queue 에 접근하여 데이터를 Dequeue 하는 Repository로서의 manager 파일 생성 2. Pritority Queue 코드에서 removeMax() 의 로직 수정(0번째 인덱스를 제대로 제거하도록)

---
+++ Sources/CLToaster/Common/CLToastManager.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Celan on 3/26/24.
+//
+
+import Foundation
--- Sources/CLToaster/Common/PriorityQueue.swift
+++ Sources/CLToaster/Common/PriorityQueue.swift
@@ -7,12 +7,7 @@
 
 import Foundation
 
-public struct PriorityQueue<T: Comparable> {
-//  public static func hi() {
-//    let arr = [1,2,5,4,3]
-//    print(PriorityQueue<Int>(order: <, startingValues: arr))
-//  }
-  
+struct PriorityQueue<T: Comparable> {
   fileprivate(set) var heap = [T]()
   private let ordered: (T, T) -> Bool
   
@@ -38,7 +33,7 @@ public struct PriorityQueue<T: Comparable> {
   ///   - startingValues: An array of elements to initialize the PriorityQueue with.
   init(
     order: @escaping (T, T) -> Bool,
-    startingValues: [T] = []
+    _ startingValues: [T] = []
   ) {
     self.ordered = order
     self.heap = startingValues
@@ -50,7 +45,9 @@ public struct PriorityQueue<T: Comparable> {
       parentIndex -= 1
     }
   }
-  
+}
+
+extension PriorityQueue {
   // MARK: - Helpers
   /// Add a new element onto the Priority Queue. O(log n)
   ///
@@ -93,11 +90,10 @@ public struct PriorityQueue<T: Comparable> {
   /// Remove and return the element with the **highest priority** or **lowest** if ascending. O(log n)
   ///
   /// - Returns: The element with the highest priority in the Priority Queue, or nil if the Priority Queue is empty.
-  public mutating func pop() -> T? {
+  mutating func pop() -> T? {
     if heap.isEmpty { return nil }
     let count = heap.count
     if count == 1 { return heap.removeFirst() }
-    fastPop(newCount: count - 1)
     
     return heap.removeLast()
   }
@@ -142,33 +138,11 @@ public struct PriorityQueue<T: Comparable> {
     }
   }
   
-  /// Helper function for pop.
-  ///
-  /// Swaps the first and last elements, then sinks the first element.
-  ///
-  /// After executing this function, calling `heap.removeLast()` returns the popped element.
-  /// - Parameter newCount: The number of elements in heap after the `pop()` operation is complete.
-  private mutating func fastPop(newCount: Int) {
-    var index = 0
-    var heap = heap
-    heap.withUnsafeMutableBufferPointer { bufferPointer in
-      let _heap = bufferPointer.baseAddress! // guaranteed non-nil because count > 0
-      swap(&_heap[0], &_heap[newCount])
-      while 2 * index + 1 < newCount {
-        var j = 2 * index + 1
-        if j < (newCount - 1) && ordered(_heap[j], _heap[j+1]) { j += 1 }
-        if !ordered(_heap[index], _heap[j]) { return }
-        swap(&_heap[index], &_heap[j])
-        index = j
-      }
-    }
-  }
-  
-  mutating func removeMax() -> T? {
+  mutating func removeFirst() -> T? {
     let max = heap[0]
     heap.swapAt(0, heap.count - 1)
     sink(0)
-    return max
+    return heap.removeFirst()
   }
 }
 
@@ -195,8 +169,9 @@ extension PriorityQueue {
     let currentNode = heap[index]
     
     while index > 0, ordered(parent, currentNode) {
-      heap.swapAt((index - 1) / 2, index)
-      index = (index - 1) / 2
+      var parentIndex = (index - 1) / 2
+      heap.swapAt(parentIndex, index)
+      index = parentIndex
     }
   }
 }
